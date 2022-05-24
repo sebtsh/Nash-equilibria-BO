9684b39f0dd8c93131502cdd162e628a46d461f4
seb <seb.tsh@gmail.com>
2022-05-24 16:21:47 +0800

    Modify run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
 echo "exp_type: $1"
 echo "utility_name: $2";
 echo "acq_name: $3";
+echo "start seed: $4";
+echo "end seed: $5";
 
 mkdir out
 
 if [[ "$1" == "pne" ]]
 then
-  for i in {1..4}
+  for ((i=$4; i<=$5; i++))
   do
-    echo "Running PNE with ""$2"" acq_name=""$3"" seed=""$i"""
-    CUDA_VISIBLE_DEVICES=-1 nohup python pnebo.py with "$2" acq_name="$3" seed="$i" > out/"""$1""_""$2""_""$3""_seed""$i""" &
+    echo "Running PNE with $2 acq_name=$3 seed=$i"
+    CUDA_VISIBLE_DEVICES=-1 nohup python pnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i" &
   done
 elif [[ "$1" == "mne" ]]
 then
-    for i in {1..4}
+    for ((i=$4; i<=$5; i++))
   do
-    echo "Running MNE with ""$2"" acq_name=""$3"" seed=""$i"""
-    CUDA_VISIBLE_DEVICES=-1 nohup python mnebo.py with "$2" acq_name="$3" seed="$i" > out/"""$1""_""$2""_""$3""_seed""$i""" &
+    echo "Running MNE with $2 acq_name=$3 seed=$i"
+    CUDA_VISIBLE_DEVICES=-1 nohup python mnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i" &
   done
 fi
