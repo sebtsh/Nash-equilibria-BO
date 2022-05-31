61c69fd027c58386245adb2cc566f3307ca7682f
seb <seb.tsh@gmail.com>
2022-05-31 21:46:51 +0800

    Change run.sh

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ then
   for ((i=$4; i<=$5; i++))
   do
     echo "Running PNE with $2 acq_name=$3 seed=$i"
-    CUDA_VISIBLE_DEVICES=-1 nohup python pnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i" &
+    CUDA_VISIBLE_DEVICES=-1 nohup python pnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i.txt" &
   done
 elif [[ "$1" == "mne" ]]
 then
     for ((i=$4; i<=$5; i++))
   do
     echo "Running MNE with $2 acq_name=$3 seed=$i"
-    CUDA_VISIBLE_DEVICES=-1 nohup python mnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i" &
+    CUDA_VISIBLE_DEVICES=-1 nohup python mnebo.py with "$2" acq_name="$3" seed="$i" > out/"$1_$2_$3_seed$i.txt" &
   done
 fi
