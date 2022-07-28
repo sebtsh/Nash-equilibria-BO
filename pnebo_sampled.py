ecc76f91360e39ce4fba19f7c7ff97edf6a10166
seb <seb.tsh@gmail.com>
2022-07-28 13:14:55 +0800

    Change pnebo_sampled.py to handle all acqs and seeds

--- pnebo_sampled.py
+++ pnebo_sampled.py
@@ -28,7 +28,6 @@ ex.observers.append(FileStorageObserver("./runs"))
 @ex.named_config
 def rand():
     utility_name = "rand"
-    acq_name = "ucb_pne"
     agent_dims = [1, 1]  # this determines num_agents and dims
     ls = np.array([0.5] * sum(agent_dims))
     bound = [-1.0, 1.0]  # assumes same bounds for all dims
@@ -37,7 +36,6 @@ def rand():
     num_iters = 200
     beta = 2.0
     n_samples_outer = 50
-    seed = 0
     known_best_val = None
     num_actions_discrete = 16
     inner_max_mode = "sample_n_shrink"
@@ -47,7 +45,6 @@ def rand():
 @ex.named_config
 def gan():
     utility_name = "gan"
-    acq_name = "ucb_pne"
     agent_dims = [2, 3]  # this determines num_agents and dims
     ls = np.array([0.5, 0.5, 2.0, 2.0, 2.0])
     bound = [-1.0, 1.0]  # assumes same bounds for all dims
@@ -56,7 +53,6 @@ def gan():
     num_iters = 600
     beta = 2.0
     n_samples_outer = 50
-    seed = 0
     known_best_val = 0.0
     num_actions_discrete = 32
     inner_max_mode = "sample_n_shrink"
@@ -66,7 +62,6 @@ def gan():
 @ex.named_config
 def bcad():
     utility_name = "bcad"
-    acq_name = "ucb_pne"
     agent_dims = [3, 2]  # this determines num_agents and dims
     ls = np.array([1.5, 0.5, 1.0, 0.5, 0.5])
     bound = [-1.0, 1.0]  # assumes same bounds for all dims
@@ -75,7 +70,6 @@ def bcad():
     num_iters = 600
     beta = 2.0
     n_samples_outer = 50
-    seed = 0
     known_best_val = 0.0
     num_actions_discrete = 32
     inner_max_mode = "sample_n_shrink"
@@ -85,7 +79,6 @@ def bcad():
 @ex.automain
 def main(
     utility_name,
-    acq_name,
     agent_dims,
     ls,
     bound,
@@ -94,7 +87,6 @@ def main(
     num_iters,
     beta,
     n_samples_outer,
-    seed,
     known_best_val,
     num_actions_discrete,
     inner_max_mode,
@@ -109,75 +101,79 @@ def main(
     dims = np.sum(agent_dims)
     bounds = np.array([bound for _ in range(dims)])
     agent_dims_bounds = get_agent_dims_bounds(agent_dims=agent_dims)
-    rng = np.random.default_rng(seed)
-    tf.random.set_seed(seed)
-    base_dir = "results/pne/" + utility_name + "/"
-    pickles_save_dir = base_dir + "pickles/"
-    Path(pickles_save_dir).mkdir(parents=True, exist_ok=True)
-    filename = f"pne-{utility_name}-{acq_name}-seed{seed}"
 
-    kernel = gpf.kernels.SquaredExponential(lengthscales=ls)
-    u, _ = get_utilities(
-        utility_name=utility_name,
-        num_agents=num_agents,
-        bounds=bounds,
-        rng=rng,
-        kernel=kernel,
-    )
+    for seed in range(5):
+        rng = np.random.default_rng(seed)
+        tf.random.set_seed(seed)
+        base_dir = "results/pne/" + utility_name + "/"
+        pickles_save_dir = base_dir + "pickles/"
+        Path(pickles_save_dir).mkdir(parents=True, exist_ok=True)
+        filename = f"pne-{utility_name}-{acq_name}-seed{seed}"
 
-    base_dir = "results/pne/" + utility_name + "/"
-    save_dir = base_dir
-    pickles_dir = base_dir + "pickles/"
-    filename = f"pne-{utility_name}-{acq_name}-seed{seed}.p"
-    (
-        reported_strategies,
-        sampled_strategies,
-        reported_sample_regret,
-        reported_cumu_regret,
-        time_per_iter,
-        args,
-    ) = pickle.load(open(pickles_dir + filename, "rb"))
+        kernel = gpf.kernels.SquaredExponential(lengthscales=ls)
+        u, _ = get_utilities(
+            utility_name=utility_name,
+            num_agents=num_agents,
+            bounds=bounds,
+            rng=rng,
+            kernel=kernel,
+        )
 
-    print("Sanity check")
-    reported_sample_regret2, reported_cumu_regret2 = calc_regret_pne(
-        u=u,
-        data=reported_strategies[:5],
-        bounds=bounds,
-        agent_dims_bounds=agent_dims_bounds,
-        mode=maxmin_mode,
-        rng=rng,
-        known_best_val=known_best_val,
-    )
+        base_dir = "results/pne/" + utility_name + "/"
+        save_dir = base_dir
+        pickles_dir = base_dir + "pickles/"
 
-    if np.allclose(reported_sample_regret2, reported_sample_regret[:5]):
-        print("Sanity check passed")
-    else:
-        print("Sanity check failed")
-        print(f"Previously computed regrets: {reported_sample_regret[:5]}")
-        print(f"Newly computed regrets: {reported_sample_regret2}")
-        raise Exception
+        for acquisition in ["ucb_pne", "prob_eq", "BN"]:
+            filename = f"pne-{utility_name}-{acquisition}-seed{seed}.p"
+            (
+                reported_strategies,
+                sampled_strategies,
+                reported_sample_regret,
+                reported_cumu_regret,
+                time_per_iter,
+                args,
+            ) = pickle.load(open(pickles_dir + filename, "rb"))
 
-    print("Computing regret for sampled strategies")
-    sampled_sample_regret, sampled_cumu_regret = calc_regret_pne(
-        u=u,
-        data=sampled_strategies,
-        bounds=bounds,
-        agent_dims_bounds=agent_dims_bounds,
-        mode=maxmin_mode,
-        rng=rng,
-        known_best_val=known_best_val,
-    )
+            print("Sanity check")
+            reported_sample_regret2, reported_cumu_regret2 = calc_regret_pne(
+                u=u,
+                data=reported_strategies[:5],
+                bounds=bounds,
+                agent_dims_bounds=agent_dims_bounds,
+                mode=maxmin_mode,
+                rng=rng,
+                known_best_val=known_best_val,
+            )
 
-    pickle.dump(
-        (
-            reported_strategies,
-            sampled_strategies,
-            reported_sample_regret,
-            reported_cumu_regret,
-            sampled_sample_regret,
-            sampled_cumu_regret,
-            time_per_iter,
-            args,
-        ),
-        open(pickles_save_dir + filename + "-2" + ".p", "wb"),
-    )
+            if np.allclose(reported_sample_regret2, reported_sample_regret[:5]):
+                print("Sanity check passed")
+            else:
+                print("Sanity check failed")
+                print(f"Previously computed regrets: {reported_sample_regret[:5]}")
+                print(f"Newly computed regrets: {reported_sample_regret2}")
+                raise Exception
+
+            print("Computing regret for sampled strategies")
+            sampled_sample_regret, sampled_cumu_regret = calc_regret_pne(
+                u=u,
+                data=sampled_strategies,
+                bounds=bounds,
+                agent_dims_bounds=agent_dims_bounds,
+                mode=maxmin_mode,
+                rng=rng,
+                known_best_val=known_best_val,
+            )
+
+            pickle.dump(
+                (
+                    reported_strategies,
+                    sampled_strategies,
+                    reported_sample_regret,
+                    reported_cumu_regret,
+                    sampled_sample_regret,
+                    sampled_cumu_regret,
+                    time_per_iter,
+                    args,
+                ),
+                open(pickles_save_dir + filename[:-2] + "-2" + ".p", "wb"),
+            )
